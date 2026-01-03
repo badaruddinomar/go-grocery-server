3de2b0e4026e3e45596abe72c76cb9d282fb5e1b
Omar-dev-xponent <omar12.xponent@gmail.com>
2026-01-03 19:53:14 +0600

    feat: auth auth guard and role guard

---
+++ src/middlewares/authGuard.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction, RequestHandler } from 'express';
+import { AppError } from '@/utils/appError';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import httpStatus from 'http-status';
+import { User } from '@/generated/prisma';
+import { prisma } from '@/utils/prismaClient';
+import envConfig from '@/config/env.config';
+
+export const authMiddleware: RequestHandler = async (
+  req: Request,
+  _res: Response,
+  next: NextFunction,
+) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    throw new AppError(
+      'Please login to access this resource.',
+      httpStatus.UNAUTHORIZED,
+    );
+  }
+
+  const decodedData = jwt.verify(token, envConfig.jwt_secret) as JwtPayload;
+
+  const user = await prisma.user.findUnique({
+    where: { id: decodedData?.userId },
+  });
+
+  if (!user) {
+    throw new AppError(
+      'User no longer exists. Please login again.',
+      httpStatus.UNAUTHORIZED,
+    );
+  }
+
+  if (!user.isVerified) {
+    throw new AppError('Please verify your email.', httpStatus.UNAUTHORIZED);
+  }
+
+  req.user = user as User;
+  next();
+};
---
+++ src/middlewares/roleGuard.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import { AppError } from '@/utils/appError';
+import httpStatus from 'http-status';
+import { UserRole } from '@/generated/prisma/edge';
+
+// Authorize Roles--
+export const authorizeRoles = (...roles: string[]) => {
+  return (req: Request, _res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user?.role as UserRole)) {
+      throw new AppError(
+        `Role ${req.user?.role} is not allowed to access this resource.`,
+        httpStatus.FORBIDDEN,
+      );
+    }
+    next();
+  };
+};
