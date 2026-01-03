36c4437cb0dc9549293f70e881a37fd6c799e47c
badaruddinomar <badaruddinomar2403@gmail.com>
2026-01-03 20:59:59 +0600

    feat: send email functionality

---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,29 @@
+import envConfig from '@/config/env.config';
+import nodemailer from 'nodemailer';
+
+interface IEmailOptions {
+  reciverEmail: string;
+  subject: string;
+  body: string;
+}
+const sendEmail = async (options: IEmailOptions) => {
+  const transporter = nodemailer.createTransport({
+    // host: envConfig.smtp_host,
+    // port: envConfig.smtp_port,
+    service: envConfig.smtp_service,
+    auth: {
+      user: envConfig.smtp_mail,
+      pass: envConfig.smtp_password,
+    },
+  });
+  const mailOptions = {
+    from: envConfig.smtp_mail,
+    to: options.reciverEmail,
+    subject: options.subject,
+    html: options.body,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+export default sendEmail;
